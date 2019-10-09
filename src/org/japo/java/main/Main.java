74c6a561a4682b2303819844db46711ac41f1da9
victo <victo@192.168.1.134>
2019-10-09 23:26:19 +0200

    :tada: Commit inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,34 @@
+/* 
+ * Copyright 2019 Elísabet Palomino .
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Elísabet Palomino
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.printf ("La fecha de Inicio es : %02d/%02d/%d%n",9,9,2019);    //System es una clase,out es un recurso de la clase System. el punto indica el recurso.  
+           
+             
+        
+        
+    }//main
+    
+}//class
